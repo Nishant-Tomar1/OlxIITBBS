8a22c6082a70d20ebc2d6f2511757cec83bdb236
Nishant <nishanttomar910@gmail.com>
2024-07-08 00:01:48 +0530

    deployement test

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,6 @@ const app = express()
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
-    // origin : ["http://localhost:3001"],
     credentials : true
 }))
 app.use(express.json({limit : "16kb"}))
