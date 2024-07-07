c23b56dda05058e8cdb8fd3b100117b0b554e576
Nishant <nishanttomar910@gmail.com>
2024-07-08 00:56:59 +0530

    testing deployment using proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,22 +5,22 @@ import bodyParser from 'body-parser'
 
 const app = express()
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}))
+// app.use(cors({
+//     origin : process.env.CORS_ORIGIN,
+//     credentials : true
+// }))
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({
     extended : true,
     limit : "16kb"
 }))
-app.use(express.static("public"))
+app.use(express.static("build"))
 app.use(cookieParser())
 app.use(bodyParser.json())
 
-app.get('/', ( __, res) => {
-    res.send('Working Successfully')
-  })
+// app.get('/', ( __, res) => {
+//     res.send('Working Successfully')
+//   })
 
 //routes
 import userRouter from "./routes/user.routes.js"
--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,5 +1,5 @@
 // export const Server = "http://localhost:8000/api/v1";
 // export const ServerBase = "http://localhost:5000"
 
-export const Server = "https://olxiitbbs.onrender.com/api/v1";
-export const ServerBase = "https://olxiitbbs.onrender.com"
+export const Server = "/api/v1";
+export const ServerBase = ""
